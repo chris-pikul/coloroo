2f69a446fd87b4b32103394e3f05034a0fdf3d55
Chris Pikul <chris@workmaple.com>
2022-01-18 06:23:14 -0800

    Added new regular expressions and utility function.

--- src/utils/regexp.ts
+++ src/utils/regexp.ts
@@ -11,6 +11,34 @@
  */
 
 /**
+ * Runs a regular expression against an input string, capturing all groups.
+ * The return is an array of arrays, where the inner arrays are all the capture
+ * groups.
+ * 
+ * @param regexp Regular Expression to use
+ * @param str Input string to match against
+ * @returns Array of arrays containing strings or undefined objects
+ */
+export function captureAll(regexp:RegExp, str:string):Array<(string|undefined)[]> {
+  const matches = [ ...str.matchAll(regexp) ];
+  return matches.map(match => match.slice(1));
+}
+
+/**
+ * Runs a regular expression against an input string, returning only the 
+ * capture groups within that first match. If no matches are made then null
+ * is returned
+ * 
+ * @param regexp Regular Expression to use 
+ * @param str Input string to match against
+ * @returns Either null, or an array of the matches
+ */
+export function captureFirst(regexp:RegExp, str:string):(Array<string|undefined> | null) {
+  const arr = captureAll(regexp, str);
+  return arr[0] ?? null;
+}
+
+/**
  * Regular expression matching (with capture groups) for hexidecimal color
  * codes.
  */
@@ -36,3 +64,16 @@ export const regexpValidateParams = /^[\d.%]+(?:(?:\s*,\s*)(?:[\d.%]+))+$/i;
  * Regular expression capturing values within a parameter string.
  */
 export const regexpParams = /([\d.%]+)/gi;
+
+/**
+ * Regular expression matching a valid number as of CSS4 standards
+ */
+export const regexpNumber = /^(?!e)[+-]?(?![+-])\d*\.?\d+(?:e[+-]?\d+)?$/i;
+
+/**
+ * Regular expression matching the CSS4 definition of RGB(A) functional
+ * notation. Features named capture groups for the components. As defined with
+ * the spec, both the rgb() and rgba() variations accept 4 components. This
+ * additionally accepts the new "none" keyword.
+ */
+export const regexpRGBFunc = /^rgba?\(\s*(?<red>none|(?!e)[+-]?(?![+-])\d*\.?\d+(?:e[+-]?\d+)?|\d*\.?\d+%)\s?,?\s+(?<green>none|(?!e)[+-]?(?![+-])\d*\.?\d+(?:e[+-]?\d+)?|\d*\.?\d+%)\s?,?\s+(?<blue>none|(?!e)[+-]?(?![+-])\d*\.?\d+(?:e[+-]?\d+)?|\d*\.?\d+%)(?:\s*[,/]\s+(?<alpha>none|(?!e)[+-]?(?![+-])\d*\.?\d+(?:e[+-]?\d+)?|\d*\.?\d+%))?\s*\)$/gi;
