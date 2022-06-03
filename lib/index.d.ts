c71703bcd116e497850e3216277ef28909345f62
Chris Pikul <chris@workmaple.com>
2022-06-02 21:51:32 -0700

    removed lib folder

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Copyright © 2021 Chris Pikul.
- *
- * This code base (coloroo) is under the MIT license. See the file at the
- * project root "LICENSE" for more information.
- * -----------------------------------------------------------------------------
- */
-export { ColorRGB } from './RGB';
